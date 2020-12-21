c7e0e7a890eab165c9daf5573c7a84b6ac780017
PY <pyhuang97@gmail.com>
2020-12-22 02:37:43 +0800

    implement simple regression report.

--- utils/mlmodelwrapper.py
+++ utils/mlmodelwrapper.py
@@ -1,4 +1,5 @@
 #%%
+import math
 import threading
 from datetime import datetime
 from sklearn.metrics import classification_report
@@ -17,6 +18,8 @@ from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
 from sklearn.utils import all_estimators
 from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
+
 
 # TODO: use different hyperparameters according to data or try out a few settings and find best of it.
 # models
@@ -55,20 +58,22 @@ class MLModelWrapper:
             X_np, y_np, test_size=0.25, random_state=RANDOM_SEED
         )
 
+    # TODO: finished implement cluster and transformer
     # filter_type can be one of the type ['classifier', 'regressor', 'cluster', 'transformer']
     def quick_test(self, filter_type="classifier", max_threads=5, save=True):
         print("*Quick test for multiple classification models!")
         threads = []
-        for name, clf_class in all_estimators(filter_type):
+        for name, estimator_class in all_estimators(filter_type):
             print(f"*start training: {name} model.")
             try:
-                clf = clf_class()
+                model = estimator_class()
                 thread = TrainModelThread(
                     self.train_X,
                     self.train_y,
                     self.test_X,
                     self.test_y,
-                    clf,
+                    model,
+                    filter_type,
                     name,
                     save,
                 )
@@ -83,29 +88,58 @@ class MLModelWrapper:
 
 
 class TrainModelThread(threading.Thread):
-    def __init__(self, train_X, train_y, test_X, test_y, clf, name=None, save=True):
+    def __init__(
+        self,
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+        model,
+        model_type="classifier",
+        name=None,
+        save=True,
+    ):
         threading.Thread.__init__(self)
-        self.train_X = train_X
-        self.train_y = train_y
-        self.test_X = test_X
-        self.test_y = test_y
-        self.clf = clf
+        self.train_X = X_train
+        self.train_y = y_train
+        self.test_X = X_test
+        self.test_y = y_test
+        self.model = model
+        self.model_type = model_type
         self.name = name
         self.save = save
 
     def run(self):
-        self.clf.fit(self.train_X, self.train_y)
-        y_pred = self.clf.predict(self.test_X)
-        report = classification_report(self.test_y, y_pred)
+        self.model.fit(self.train_X, self.train_y)
+        y_pred = self.model.predict(self.test_X)
+        if self.model_type == "classifier":
+            report = classification_report(self.test_y, y_pred)
+        elif self.model_type == "regressor":
+            report = regression_report(self.test_y, y_pred)
         if self.name != None:
             print(f"Method: {self.name}")
         print(report)
         if self.save:
-            print("*Append result to ML_Classifiers_Report.txt")
-            with open("ML_Classifiers_Report.txt", "a") as ofile:
+            print(f"*Append result to SKLearn_{self.model_type}s_Report.txt")
+            with open(f"SKLearn_{self.model_type}s_Report.txt", "a") as ofile:
                 if self.name != None:
                     ofile.write(f"Method: {self.name}\n")
                 ofile.write(f"finished time: {datetime.now()}\n")
                 ofile.write(report)
                 ofile.write("-" * 20 + "\n")
         print("-" * 20)
+
+
+# report for regression result
+def regression_report(y_true, y_pred):
+    r2 = metrics.r2_score(y_true, y_pred)
+    max_error = metrics.max_error(y_true, y_pred)
+    mse = metrics.mean_squared_error(y_true, y_pred)
+    mean_absolute_error = metrics.mean_absolute_error(y_true, y_pred)
+    report = [
+        f"r2: {r2:.3f}",
+        f"MSE: {mse:.3f}",
+        f"RMSE: {math.sqrt(mse):.3f}",
+        f"Max Error: {max_error:.3f}",
+    ]
+    return "\n".join(report) + "\n"
