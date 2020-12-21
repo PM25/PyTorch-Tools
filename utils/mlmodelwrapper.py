dd2707c2a673305e52c022e759b438474c5dedc7
PY <pyhuang97@gmail.com>
2020-12-22 02:55:06 +0800

    add adjusted r2 metric to regression_report

--- utils/mlmodelwrapper.py
+++ utils/mlmodelwrapper.py
@@ -115,7 +115,7 @@ class TrainModelThread(threading.Thread):
         if self.model_type == "classifier":
             report = classification_report(self.test_y, y_pred)
         elif self.model_type == "regressor":
-            report = regression_report(self.test_y, y_pred)
+            report = regression_report(self.test_y, y_pred, self.test_X.shape[1])
         if self.name != None:
             print(f"Method: {self.name}")
         print(report)
@@ -131,13 +131,18 @@ class TrainModelThread(threading.Thread):
 
 
 # report for regression result
-def regression_report(y_true, y_pred):
+def regression_report(y_true, y_pred, nfeatures=None):
     r2 = metrics.r2_score(y_true, y_pred)
     max_error = metrics.max_error(y_true, y_pred)
     mse = metrics.mean_squared_error(y_true, y_pred)
     mean_absolute_error = metrics.mean_absolute_error(y_true, y_pred)
+    if nfeatures is not None:
+        adjusted_r2 = 1 - (1 - r2) * (len(y_true) - 1) / (len(y_true) - nfeatures - 1)
+    else:
+        adjusted_r2 = None
     report = [
-        f"r2: {r2:.3f}",
+        f"R2: {r2:.3f}",
+        f"Adj. R2: {adjusted_r2:.3f}",
         f"MSE: {mse:.3f}",
         f"RMSE: {math.sqrt(mse):.3f}",
         f"Max Error: {max_error:.3f}",
